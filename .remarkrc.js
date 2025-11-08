53216f317907d0871bd16018f707397a8c5a6986
Eric Mika <eric@ericmika.com>
2025-11-08 15:53:02 -0500

    Root project passes lint.

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,5 +1,3 @@
 import { remarkConfig } from '@kitschpatrol/remark-config'
 
-export default remarkConfig({
-	rules: [['remarkValidateLinks', { repository: false }]],
-})
+export default remarkConfig()
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,6 @@
+import type { TypedFlatConfigItem } from '@kitschpatrol/eslint-config'
 import { eslintConfig } from '@kitschpatrol/eslint-config'
+import { TEMPLATE_TYPES } from './src/template'
 
 export default eslintConfig(
 	{
@@ -6,14 +8,26 @@ export default eslintConfig(
 		// astro: true,
 		// react: true,
 		// svelte: true,
+		// No other way around "Unused eslint-disable directive"
+		ignores: ['templates/electron/electron/electron-env.d.ts'],
 		type: 'lib',
 	},
-	{
-		basePath: 'templates/cli',
-		files: ['package.json'],
+	...templateOverrides(),
+)
+
+// Only way to quiet programmatic lint output, but still doesn't apply to the
+// vscode plugin
+function templateOverrides(): TypedFlatConfigItem[] {
+	return TEMPLATE_TYPES.map((type) => ({
+		basePath: `templates/${type}`,
 		rules: {
+			'import/no-unresolved': 'off',
 			'json-package/valid-author': 'off',
 			'json-package/valid-name': 'off',
+			'mdx/remark': 'off',
+			'ts/consistent-type-definitions': 'off',
+			'unicorn/no-abusive-eslint-disable': 'off',
+			'unicorn/no-process-exit': 'off',
 		},
-	},
-)
+	}))
+}
